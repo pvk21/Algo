db0cb4918e8663fb107f7b572a17bb576f38b7b2
pranav <kulkarnipranav96@gmail.com>
2019-04-07 21:33:54 +0530

    Completed Heap Sort

--- Algo/HeapSort.cpp
+++ Algo/HeapSort.cpp
@@ -4,12 +4,24 @@ void max_heapify(int*, int, int);
 void build_max_heap(int* , int);
 void runAlgo::Heapsort()
 {
-	int array[] = { 10,90,7,100,4,78,203};
+	int array[] = { 4,1,3,2,16,9,10,14,18,7};
 	int n = sizeof(array) / sizeof(array[0]);
-	build_max_heap(array, n);
-	for (int i = 0; i < 7; i++)
+	for (int i = n / 2 - 1; i >= 0; i--)
+		max_heapify(array, n, i);
+
+	// One by one extract an element from heap 
+	for (int i = n - 1; i >= 0; i--)
 	{
-		std::cout << array[i]<<"\n";
+		// Move current root to end 
+		std::swap(array[0], array[i]);
+
+		// call max heapify on the reduced heap 
+		max_heapify(array, i, 0);
+	}
+	std::cout << "Sorted Array using Heapsort";
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << "\n" << array[i];
 	}
 }
 void max_heapify(int* numArray,int n,int i)
@@ -17,12 +29,12 @@ void max_heapify(int* numArray,int n,int i)
 	int Largest = i;
 	int Left = 2 * i + 1;
 	int Right = 2 * i + 2;
-	if (Left < n && numArray[Left] > Largest)
+	if (Left < n && numArray[Left] > numArray[Largest])
 	{
 		Largest = Left;
 	}
 	
-	if (Right < n && numArray[Left] > numArray[Largest])
+	if (Right < n && numArray[Right] > numArray[Largest])
 	{
 		Largest = Right;
 	}
@@ -31,11 +43,4 @@ void max_heapify(int* numArray,int n,int i)
 		std::swap(numArray[i], numArray[Largest]);
 		max_heapify(numArray, n, Largest);
 	}	
-}
-void build_max_heap(int *numArray,int n)
-{
-	for (int i = n/2 - 1; i > 0; i--)
-	{
-		max_heapify(numArray, n, i);
-	}
 }
