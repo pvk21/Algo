1dd2ea5679e1b89e2fa98c533f8ca9abff44cd2b
pranav <kulkarnipranav96@gmail.com>
2019-04-09 11:09:42 +0530

    added class for operations on Heap

--- Algo/HeapSort.cpp
+++ Algo/HeapSort.cpp
@@ -1,10 +1,9 @@
 #include "RunAlgo.h"
 #include<iostream>
 void max_heapify(int*, int, int);
-void build_max_heap(int* , int);
 void runAlgo::Heapsort()
 {
-	int array[] = { 4,1,3,2,16,9,10,14,18,7};
+	int array[] = {16,14,10,8,7,9,3,2,4,1};
 	int n = sizeof(array) / sizeof(array[0]);
 	for (int i = n / 2 - 1; i >= 0; i--)
 		max_heapify(array, n, i);
@@ -24,23 +23,4 @@ void runAlgo::Heapsort()
 		std::cout << "\n" << array[i];
 	}
 }
-void max_heapify(int* numArray,int n,int i)
-{
-	int Largest = i;
-	int Left = 2 * i + 1;
-	int Right = 2 * i + 2;
-	if (Left < n && numArray[Left] > numArray[Largest])
-	{
-		Largest = Left;
-	}
-	
-	if (Right < n && numArray[Right] > numArray[Largest])
-	{
-		Largest = Right;
-	}
-	if (Largest != i)
-	{	
-		std::swap(numArray[i], numArray[Largest]);
-		max_heapify(numArray, n, Largest);
-	}	
-}
+
