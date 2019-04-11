029f9ac01e77159aefb37ac9c242285e610fef42
pranav <kulkarnipranav96@gmail.com>
2019-04-11 15:36:24 +0530

    Revert "Operartions on heap"
    This reverts commit 1c260ce756fda02fd7a9c9058260bdf127ac4040.

--- Algo/HeapSort.cpp
+++ Algo/HeapSort.cpp
@@ -1,10 +1,13 @@
 #include "RunAlgo.h"
 #include<iostream>
-#include<vector>
-std::vector<int> runAlgo::Heapsort(std::vector<int> array,int n)
+void max_heapify(int*, int, int);
+void build_max_heap(int* , int);
+void runAlgo::Heapsort()
 {
+	int array[] = { 4,1,3,2,16,9,10,14,18,7};
+	int n = sizeof(array) / sizeof(array[0]);
 	for (int i = n / 2 - 1; i >= 0; i--)
-		runAlgo::max_heapify(array, n, i);
+		max_heapify(array, n, i);
 
 	// One by one extract an element from heap 
 	for (int i = n - 1; i >= 0; i--)
@@ -13,9 +16,31 @@ std::vector<int> runAlgo::Heapsort(std::vector<int> array,int n)
 		std::swap(array[0], array[i]);
 
 		// call max heapify on the reduced heap 
-		runAlgo::max_heapify(array, i, 0);
+		max_heapify(array, i, 0);
 	}
-	std::cout << "Sorted Array using Heapsort\n";
-	return array;
+	std::cout << "Sorted Array using Heapsort";
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << "\n" << array[i];
+	}
 }
-
+void max_heapify(int* numArray,int n,int i)
+{
+	int Largest = i;
+	int Left = 2 * i + 1;
+	int Right = 2 * i + 2;
+	if (Left < n && numArray[Left] > numArray[Largest])
+	{
+		Largest = Left;
+	}
+	
+	if (Right < n && numArray[Right] > numArray[Largest])
+	{
+		Largest = Right;
+	}
+	if (Largest != i)
+	{	
+		std::swap(numArray[i], numArray[Largest]);
+		max_heapify(numArray, n, Largest);
+	}	
+}
--- Algo/RunAlgo.h
+++ Algo/RunAlgo.h
@@ -1,13 +1,7 @@
 #pragma once
-#include<iostream>
-#include<vector>
 class runAlgo
 {
 public:
-	std::vector<int> Heapsort(std::vector<int> ,int);
-	void static max_heapify(std::vector<int>, int , int );
-	int max_element(std::vector<int> );
-	void increase_key(std::vector<int>, int, int);
-	int parent(int);
-	void insert_key(std::vector<int>, int,size_t);
+	void Heapsort();
+	void insertionSort();
 };
